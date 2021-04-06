2e21c6c211cdb651ce7507fb6d9aec35ffcf8661
9916103020 <divyansh.srivastavadps@gmail.com>
2021-04-06 22:32:50 +0530

    Just to deploy

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -123,10 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "travel/static")
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "travel/static")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "travel/media")
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "travel/media")
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
